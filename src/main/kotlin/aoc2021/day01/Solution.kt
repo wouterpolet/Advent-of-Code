a086a285ba327d7888446d210926e8adceb1c51c
Wouter Polet <wouterpolet@gmail.com>
2021-12-01 07:07:29 +0100

    Add day 1 of 2021

--- src/main/kotlin/aoc2021/day01/Solution.kt
+++ src/main/kotlin/aoc2021/day01/Solution.kt
@@ -6,17 +6,20 @@ val year = 2021
 val day = 1
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>): Int = input
+        .map { it.toInt() }
+        .zipWithNext { a, b -> b > a }
+        .count { it }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>): Int = input
+        .map { it.toInt() }
+        .windowed(3) { it.sum() }
+        .zipWithNext { a, b -> b > a }
+        .count { it }
 
-//    val testInput = readInput(year, day, Input.Test)
-//    check(part1(testInput) == 1)
-//    check(part2(testInput) == 1)
+    val testInput = readInput(year, day, Input.Test)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput(year, day, Input.Real)
     println("Day $day, part one: ${part1(input)}")
