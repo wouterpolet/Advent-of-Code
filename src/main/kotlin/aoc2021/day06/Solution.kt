38f461e04b2d1fcc92f72a2ec55957df23146304
Wouter Polet <wouterpolet@gmail.com>
2021-12-06 07:30:40 +0100

    Add day 5 and 6 of 2021

--- src/main/kotlin/aoc2021/day06/Solution.kt
+++ src/main/kotlin/aoc2021/day06/Solution.kt
@@ -7,16 +7,42 @@ val day = 6
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val fish = input.first().split(',').map { listOf(it.toInt(), 6).toMutableList() }.toMutableList()
+        for (i in 1..80) {
+            val addFish = listOf<MutableList<Int>>().toMutableList()
+            for (f in fish) {
+                if (f[0] == 0) {
+                    addFish.add(listOf(f[1] + 2, 6).toMutableList())
+                    f[0] = f[1]
+                } else {
+                    f[0]--
+                }
+            }
+            fish.addAll(addFish)
+        }
+        return fish.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val fish = HashMap<Int, Long>().toMutableMap()
+        input.first().split(',').forEach { fish[it.toInt()] = fish.getOrDefault(it.toInt(), 0) + 1 }
+        for (f in 1..8) {
+            fish[f] = fish.getOrDefault(f, 0)
+        }
+        for (k in 1..256) {
+            val temp = fish.getOrDefault(0, 0)
+            for (f in 1..8) {
+                fish[f-1] = fish[f]!!
+            }
+            fish[8] = temp
+            fish[6] = fish[6]!! + temp
+        }
+        return fish.map { (_, v) -> v }.sum()
     }
 
-//    val testInput = readInput(year, day, Input.Test)
-//    check(part1(testInput) == 1)
-//    check(part2(testInput) == 1)
+    val testInput = readInput(year, day, Input.Test)
+    check(part1(testInput) == 5934)
+    check(part2(testInput) == 26984457539)
 
     val input = readInput(year, day, Input.Real)
     println("Day $day, part one: ${part1(input)}")
