ecb4a61984826f3ce485adbba3b8dd32a2538056
Wouter Polet <wouterpolet@gmail.com>
2021-12-18 20:33:41 +0100

    Add days 15, 16, and 18 of 2021

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -27,3 +27,7 @@ fun <T> List<T>.permutations(): List<List<T>> =
         listOf(this)
     else
         map { x -> (this - x).permutations().map { listOf(x) + it } }.flatten()
+
+fun <T> List<T>.combinations(): List<Pair<T, T>> = flatMapIndexed { i, x -> filterIndexed { index, _ -> index != i }.map { y -> Pair(x, y) } }
+
+operator fun Pair<Int, Int>.plus(p: Pair<Int, Int>): Pair<Int, Int> = Pair(first + p.first, second + p.second)
