75ebbabdb65dc5b8b3531dfa3960e8917e9bfe30
Wouter Polet <wouterpolet@gmail.com>
2021-12-25 06:26:35 +0100

    Finish advent of code 2021

--- src/main/kotlin/aoc2021/day25/Solution.kt
+++ src/main/kotlin/aoc2021/day25/Solution.kt
@@ -1,24 +1,56 @@
 package aoc2021.day25
 
+import plus
 import readInput
 
 val year = 2021
 val day = 25
 
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return 0
+        var newSouth = (0 until input.size).flatMap { y -> (0 until input[y].length).map { x -> if (input[y][x] == 'v') Pair(x, y) else null }.filterNotNull() }.toSet()
+        var newEast = (0 until input.size).flatMap { y -> (0 until input[y].length).map { x -> if (input[y][x] == '>') Pair(x, y) else null }.filterNotNull() }.toSet()
+        val height = input.size
+        val width = input[0].length
+
+        var south = emptySet<Pair<Int, Int>>()
+        var east = emptySet<Pair<Int, Int>>()
+
+        fun getCoordinate(p: Pair<Int, Int>) = Pair(p.first % width, p.second % height)
+
+        var count = 0
+
+        while (newSouth != south || newEast != east) {
+            east = newEast
+            south = newSouth
+            newSouth = mutableSetOf()
+            newEast = mutableSetOf()
+            var taken = south + east
+            for (e in east) {
+                if (!(getCoordinate(e + Pair(1, 0)) in taken)) {
+                    newEast.add(getCoordinate(e + Pair(1, 0)))
+                } else {
+                    newEast.add(e)
+                }
+            }
+            taken = south + newEast
+            for (s in south) {
+                if (!(getCoordinate(s + Pair(0, 1)) in taken)) {
+                    newSouth.add(getCoordinate(s + Pair(0, 1)))
+                } else {
+                    newSouth.add(s)
+                }
+            }
+            count++
+        }
+
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
-
-//    val testInput = readInput(year, day, Input.Test)
-//    check(part1(testInput) == 1)
-//    check(part2(testInput) == 1)
+    val testInput = readInput(year, day, Input.Test)
+    check(part1(testInput) == 58)
 
     val input = readInput(year, day, Input.Real)
     println("Day $day, part one: ${part1(input)}")
-    println("Day $day, part two: ${part2(input)}")
 }
