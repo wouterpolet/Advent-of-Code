939d83c2264e7a8ce101dae2f58024f599ce852a
Wouter Polet <wouterpolet@gmail.com>
2022-11-30 02:21:19 +0100

    Remove some files and fix gitignore

--- rust/target/debug/deps/advent_of_code-114dded4fee79d12.d
+++
@@ -1,7 +0,0 @@
-/home/wpolet/Projects/advent-of-code/rust/target/debug/deps/advent_of_code-114dded4fee79d12.rmeta: src/lib.rs
-
-/home/wpolet/Projects/advent-of-code/rust/target/debug/deps/libadvent_of_code-114dded4fee79d12.rlib: src/lib.rs
-
-/home/wpolet/Projects/advent-of-code/rust/target/debug/deps/advent_of_code-114dded4fee79d12.d: src/lib.rs
-
-src/lib.rs:
--- rust/target/debug/deps/advent_of_code-763a753f8bfa857e.d
+++
@@ -1,9 +0,0 @@
-/home/wpolet/Projects/advent-of-code/rust/target/debug/deps/advent_of_code-763a753f8bfa857e: src/main.rs src/aoc2015/mod.rs src/aoc2015/day01.rs src/aoc2015/day11.rs src/aoc2015/day12.rs
-
-/home/wpolet/Projects/advent-of-code/rust/target/debug/deps/advent_of_code-763a753f8bfa857e.d: src/main.rs src/aoc2015/mod.rs src/aoc2015/day01.rs src/aoc2015/day11.rs src/aoc2015/day12.rs
-
-src/main.rs:
-src/aoc2015/mod.rs:
-src/aoc2015/day01.rs:
-src/aoc2015/day11.rs:
-src/aoc2015/day12.rs:
